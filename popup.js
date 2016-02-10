6da68e815f92ef83e91164eb67c70997f6996ff1
Max Glenister <moglenstar@gmail.com>
2016-02-10 20:16:50 +0000

    Tweaked result fetching

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 (function() {
   'use strict';
-  var url = "https://cmx.weightwatchers.co.uk/api/v2/cmx/operations/composed/members/~/lists/memberrecipes?isActive=true&limit=2000&offset=0";
+  var url = "https://cmx.weightwatchers.co.uk/api/v2/cmx/operations/composed/members/~/lists/memberrecipes?isActive=true&limit=100&offset=";
+  var receivedCount = 0;
+  var totalHits = 0;
+  var allData = {};
 
   var getJSON = function(url, successHandler, errorHandler) {
     var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
@@ -20,36 +23,55 @@
     };
     xhr.send();
   };
+  var jsonConcat = function(o1, o2) {
+    var out = [];
+    for (var key in o1) {
+      out.push(o1[key]);
+    }
+    for (var key in o2) {
+      out.push(o2[key]);
+    }
+    return JSON.parse(JSON.stringify(out));
+  }
 
 
   var renderList = function(data) {
-    console.log('rendered');
-    var count = data.totalHits;
-    var hits = data.hits;
-    var recipeTpl = '<input class="fuzzy-search" placeholder="Search"/><ul class="list">{{#each hits}}<li><h3 class="name">{{name}} (<a href="http://cmx.weightwatchers.co.uk/#/nui/explore/member-food/details/v3:{{_id}}:{{sourceType}}:{{versionId}}" target="_blank">View</a>) ({{smartPoints}} SP)</h3></li>{{/each}}</ul>';
+    var hits = data;
+    console.log(hits[0].name);
+    var recipeTpl = '<input class="fuzzy-search" placeholder="Search"/><ul class="list">{{#each .}}<li><h3 class="name">{{name}} (<a href="http://cmx.weightwatchers.co.uk/#/nui/explore/member-food/details/v3:{{_id}}:{{sourceType}}:{{versionId}}" target="_blank">View</a>) ({{smartPoints}} SP)</h3></li>{{/each}}</ul>';
 
     var template = Handlebars.compile(recipeTpl);
-    var html = template(data);
+    var html = template(hits);
     document.querySelectorAll('#output')[0].innerHTML = html;
+    console.log(document.querySelectorAll('.list li').length);
   };
-
-  getJSON(url, function(data) {
-    renderList(data);
-    initSearch();
-  }, function(status) {
-    console.log(status);
-  });
   var initSearch = function() {
-    console.log('Search ready');
     var recipeList = new List('output', {
       valueNames: ['name'],
-      plugins: [ListFuzzySearch()],
-      indexAsync: true
+      plugins: [ListFuzzySearch()]
     });
 
     document.querySelectorAll('input')[0].focus();
   };
 
+  var successHandler = function(data) {
+    allData = jsonConcat(allData, data.hits);
 
+    if (data.totalHits) {
+      totalHits = data.totalHits;
+    }
+
+    if (receivedCount < totalHits) {
+      receivedCount = Object.keys(allData).length;
+      getJSON(url + receivedCount, successHandler, errorHandler);
+    } else {
+      renderList(allData);
+      initSearch();
+    }
+  };
+  var errorHandler = function(status) {
+    console.log(status);
+  };
+  getJSON(url + receivedCount, successHandler, errorHandler);
 
 }());
